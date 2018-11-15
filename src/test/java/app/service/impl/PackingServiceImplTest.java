08bab5ccbed733a9d332e4ee6e387370bd3c9f24
vladimir <vladimirsostaric3390@gmail.com>
2018-11-15 17:10:35 +0100

    add first test

--- src/test/java/app/service/impl/PackingServiceImplTest.java
+++ src/test/java/app/service/impl/PackingServiceImplTest.java
@@ -3,6 +3,13 @@ package app.service.impl;
 import app.service.PackingService;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class PackingServiceImplTest {
 
     private PackingService service = new PackingServiceImpl();
@@ -10,6 +17,15 @@ public class PackingServiceImplTest {
     @Test
     public void testPackingService() {
 
+
+        List<Integer> packs = new ArrayList<Integer>();
+        packs.add(3);
+        packs.add(5);
+        Map<Integer, Integer> packed = service.packOrder(10, packs);
+
+        assertNotNull(packed);
+        assertTrue(packed.size() == 1);
+        assertTrue(packed.get(5) == 2);
 
 
     }
