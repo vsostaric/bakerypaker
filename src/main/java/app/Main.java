41b84d68b0d656c69413efb62903b4f20b168eda
vladimir <vladimirsostaric3390@gmail.com>
2018-11-16 16:38:28 +0100

    add args checker

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -9,15 +9,27 @@ public class Main {
 
     public static void main(String[] args) {
 
+        boolean argsOK = checkArgs(args);
+
+        if(!argsOK) {
+            System.out.println("Invalid arguments. Two arguments needed: ordersSize and productCode.");
+            System.out.println("Example: java -jar *.jar 10 VS5");
+            return;
+        }
+
         ProductService productService = new ProductServiceImpl("products.json");
         PackingService packingService = new PackingServiceImpl(productService);
 
-        int orderSize = Integer.parseInt(args[0]);;
+        int orderSize = Integer.parseInt(args[0]);
         String productCode = args[1];
 
         System.out.println(packingService.getPackingOutput(orderSize, productCode));
 
 
+    }
+
+    private static boolean checkArgs(String[] args) {
+        return (args != null && args.length >= 2);
     }
 
 }
